ceaf6c0b371072110bbc3027effe2cb783780e2e
jose <al1970627@ite.edu.mx>
2024-08-29 21:16:32 -0700

    env

--- algoritmo.py
+++ algoritmo.py
@@ -1,4 +1,3 @@
-import numpy as np
 
 import numpy as np
 
@@ -17,19 +16,4 @@ matriz = np.array([[-3,-3,2,-3,3,-2,-2,1,2,0,2,0,1],
                     [1,-3,1,0,1,2,3,1,-2,3,3,0,3]
                    
                    ])
-etiquetas_filas = ['a', 'b', 'c', 'd', 'e', 'f','g','h','i','j','k','l','m']
-numeros_columnas = list(range(1, matriz.shape[0] + 1))
-
-print("    ", end="")
-for letra in etiquetas_filas:
-    print(f"  {letra}  ", end="")
-
-print()
-
-for i in range(matriz.shape[0]):
-    print(f"{numeros_columnas[i]:<4}", end="")
-
-    for j in range(matriz.shape[1]):
-        print(f"{matriz[i, j]:<5}", end="")
-
-    print()
+print(matriz)
